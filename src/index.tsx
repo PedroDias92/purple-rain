3c1e6cacd34485c1b2771450dc2e0faa4a58b7fe
Dias <p.m.dias@accenture.com>
2021-07-06 10:34:00 +0100

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export interface Props {}
+
+/**
+ * A custom Thing component. Neat!
+ */
+export const Thing: React.FC = ({ children }) => {
+  return <div>{children || `the snozzberries taste like snozzberries`}</div>;
+};
+
+export const SimpleStyledH1 = styled.h1`
+  color: blue;
+  border: 1px solid red;
+`;
+export const SimpleStyledH1Application = (): JSX.Element => {
+  return <SimpleStyledH1>Hi</SimpleStyledH1>;
+};
